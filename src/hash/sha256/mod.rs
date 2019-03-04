1c326f21dbbd8bfe728e9e2078a9d0cb512f655a
Michael Cordover <mjecrypto@mjec.net>
2019-03-03 19:42:38 -0500

    Remove reliance on Vec; set no_std

--- src/hash/sha256/mod.rs
+++ src/hash/sha256/mod.rs
@@ -2,61 +2,48 @@ mod magic_numbers;
 #[cfg(test)]
 mod tests;
 
-/// Hash some bytes with SHA256; output is always big endian.
-///
-/// ```rust
-/// use mjecrypto::hash::sha256::hash;
-/// assert_eq!(
-///     hash(
-///         &String::from("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq")
-///             .as_bytes()
-///     ),
-///     [
-///         0x24, 0x8d, 0x6a, 0x61, 0xd2, 0x06, 0x38, 0xb8, 0xe5, 0xc0, 0x26, 0x93, 0x0c, 0x3e,
-///         0x60, 0x39, 0xa3, 0x3c, 0xe4, 0x59, 0x64, 0xff, 0x21, 0x67, 0xf6, 0xec, 0xed, 0xd4,
-///         0x19, 0xdb, 0x06, 0xc1
-///     ]
-/// );
-/// ```
+// A struct for holding the current state of a SHA256 calculation in progress
+#[derive(Copy, Clone)]
+struct HashState {
+    result: [u32; 8],
+    message_schedule: [u32; 64],
+}
 
-#[allow(clippy::needless_range_loop)] // this is a false positive
-pub fn hash(input: &[u8]) -> [u8; 32] {
-    let mut result: [u32; 8] = magic_numbers::INITIAL_VALUE;
-    let mut message_schedule: [u32; 64] = [0; 64];
-    let mut a: u32;
-    let mut b: u32;
-    let mut c: u32;
-    let mut d: u32;
-    let mut e: u32;
-    let mut f: u32;
-    let mut g: u32;
-    let mut h: u32;
+/// Methods for calculating SHA256
+impl HashState {
+    fn new() -> HashState {
+        HashState {
+            result: magic_numbers::INITIAL_VALUE,
+            message_schedule: [0; 64],
+        }
+    }
 
-    for chunk_u8 in padded_input(input).chunks(64) {
-        let chunk: [u32; 16] = make_u32s_from_u8s(chunk_u8);
-        message_schedule[0..16].copy_from_slice(&chunk);
-        a = result[0];
-        b = result[1];
-        c = result[2];
-        d = result[3];
-        e = result[4];
-        f = result[5];
-        g = result[6];
-        h = result[7];
+    fn update(&mut self, chunk: &[u32; 16]) {
+        let mut a: u32 = self.result[0];
+        let mut b: u32 = self.result[1];
+        let mut c: u32 = self.result[2];
+        let mut d: u32 = self.result[3];
+        let mut e: u32 = self.result[4];
+        let mut f: u32 = self.result[5];
+        let mut g: u32 = self.result[6];
+        let mut h: u32 = self.result[7];
+        self.message_schedule[0..16].copy_from_slice(chunk);
 
+        // Stretching our input across the message schedule
         for j in 16..64 {
-            message_schedule[j] = little_sigma_1(message_schedule[j - 2])
-                .wrapping_add(message_schedule[j - 7])
-                .wrapping_add(little_sigma_0(message_schedule[j - 15]))
-                .wrapping_add(message_schedule[j - 16]);
+            self.message_schedule[j] = little_sigma_1(self.message_schedule[j - 2])
+                .wrapping_add(self.message_schedule[j - 7])
+                .wrapping_add(little_sigma_0(self.message_schedule[j - 15]))
+                .wrapping_add(self.message_schedule[j - 16]);
         }
 
+        // 64 rounds of mixing
         for j in 0..64 {
             let t1 = h
                 .wrapping_add(big_sigma_1(e))
                 .wrapping_add(ch(e, f, g))
                 .wrapping_add(magic_numbers::K[j])
-                .wrapping_add(message_schedule[j]);
+                .wrapping_add(self.message_schedule[j]);
             let t2 = big_sigma_0(a).wrapping_add(maj(a, b, c));
             h = g;
             g = f;
@@ -67,110 +54,174 @@ pub fn hash(input: &[u8]) -> [u8; 32] {
             b = a;
             a = t1.wrapping_add(t2);
         }
-        result[0] = a.wrapping_add(result[0]);
-        result[1] = b.wrapping_add(result[1]);
-        result[2] = c.wrapping_add(result[2]);
-        result[3] = d.wrapping_add(result[3]);
-        result[4] = e.wrapping_add(result[4]);
-        result[5] = f.wrapping_add(result[5]);
-        result[6] = g.wrapping_add(result[6]);
-        result[7] = h.wrapping_add(result[7]);
+
+        // Store the results
+        self.result[0] = a.wrapping_add(self.result[0]);
+        self.result[1] = b.wrapping_add(self.result[1]);
+        self.result[2] = c.wrapping_add(self.result[2]);
+        self.result[3] = d.wrapping_add(self.result[3]);
+        self.result[4] = e.wrapping_add(self.result[4]);
+        self.result[5] = f.wrapping_add(self.result[5]);
+        self.result[6] = g.wrapping_add(self.result[6]);
+        self.result[7] = h.wrapping_add(self.result[7]);
     }
 
-    let mut output: [u8; 32] = [0; 32];
-    output[0..4].copy_from_slice(&result[0].to_be_bytes());
-    output[4..8].copy_from_slice(&result[1].to_be_bytes());
-    output[8..12].copy_from_slice(&result[2].to_be_bytes());
-    output[12..16].copy_from_slice(&result[3].to_be_bytes());
-    output[16..20].copy_from_slice(&result[4].to_be_bytes());
-    output[20..24].copy_from_slice(&result[5].to_be_bytes());
-    output[24..28].copy_from_slice(&result[6].to_be_bytes());
-    output[28..32].copy_from_slice(&result[7].to_be_bytes());
-    output
+    fn result_bytes(&self) -> [u8; 32] {
+        let mut output: [u8; 32] = [0; 32];
+        output[0..4].copy_from_slice(&self.result[0].to_be_bytes());
+        output[4..8].copy_from_slice(&self.result[1].to_be_bytes());
+        output[8..12].copy_from_slice(&self.result[2].to_be_bytes());
+        output[12..16].copy_from_slice(&self.result[3].to_be_bytes());
+        output[16..20].copy_from_slice(&self.result[4].to_be_bytes());
+        output[20..24].copy_from_slice(&self.result[5].to_be_bytes());
+        output[24..28].copy_from_slice(&self.result[6].to_be_bytes());
+        output[28..32].copy_from_slice(&self.result[7].to_be_bytes());
+        output
+    }
+}
+
+/// Hash some bytes with SHA256; output is a string of bytes, always big endian.
+///
+/// ```rust
+/// use mjecrypto::hash::sha256::hash;
+/// assert_eq!(
+///     hash(b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"),
+///     [
+///         0x24, 0x8d, 0x6a, 0x61, 0xd2, 0x06, 0x38, 0xb8, 0xe5, 0xc0, 0x26, 0x93, 0x0c, 0x3e, 0x60, 0x39,
+///         0xa3, 0x3c, 0xe4, 0x59, 0x64, 0xff, 0x21, 0x67, 0xf6, 0xec, 0xed, 0xd4, 0x19, 0xdb, 0x06, 0xc1,
+///     ]
+/// );
+/// assert_eq!(
+///     hash(b"Hello world"),
+///     [
+///         0x64, 0xec, 0x88, 0xca, 0x00, 0xb2, 0x68, 0xe5, 0xba, 0x1a, 0x35, 0x67, 0x8a, 0x1b, 0x53, 0x16,
+///         0xd2, 0x12, 0xf4, 0xf3, 0x66, 0xb2, 0x47, 0x72, 0x32, 0x53, 0x4a, 0x8a, 0xec, 0xa3, 0x7f, 0x3c,
+///     ]
+/// );
+/// ```
+pub fn hash(input: &[u8]) -> [u8; 32] {
+    let mut state: HashState = HashState::new();
+
+    let mut input_iter = input.chunks_exact(64);
+
+    loop {
+        match input_iter.next() {
+            None => break,
+            Some(chunk_u8) => state.update(&make_16xu32s_from_64xu8s(chunk_u8)),
+        }
+    }
+
+    // Okay so now we have to pull out the remaining bits, plus whatever padding we need...
+    // It's super annoying how complex this is, but it's at most two calls to state.update()
+    // so I guess that's acceptable?
+    let remainder = input_iter.remainder();
+    let (padding_bytes, padding_byte_count) = get_padding_bytes(input.len() * 8);
+    let mut last_chunk_u8: [u8; 64] = [0; 64];
+    last_chunk_u8[0..remainder.len()].copy_from_slice(remainder);
+    let bytes_of_padding_required: usize = 64 - remainder.len();
+    last_chunk_u8[remainder.len()..64]
+        .copy_from_slice(&padding_bytes[0..bytes_of_padding_required]);
+    state.update(&make_16xu32s_from_64xu8s(&last_chunk_u8));
+    if padding_byte_count > bytes_of_padding_required {
+        last_chunk_u8.copy_from_slice(&padding_bytes[bytes_of_padding_required..]);
+        state.update(&make_16xu32s_from_64xu8s(&last_chunk_u8));
+    }
+
+    state.result_bytes()
+}
+
+/// Returns a 72-byte array filled with padding bytes, and a length which is the number of bytes
+/// of that slice that you will need. The remaining bytes will be set to 0x00. By doing this, we
+/// avoid using a vec which improves performance a little. Yes, it's possible for padding length
+/// to be up to 72 bytes.
+#[inline]
+fn get_padding_bytes(input_length_in_bits: usize) -> ([u8; 72], usize) {
+    debug_assert_eq!(input_length_in_bits % 8, 0);
+
+    let mut output: [u8; 72] = [
+        0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    ];
+
+    // number of zeros to get to a multiple of 512 bits total length, given
+    // we have to add a 1 and the length of the message in bits, as a big-endian
+    // u64.
+    let input_length_mod_512 = input_length_in_bits % 512;
+    let number_of_zeros: usize =
+        447 + if input_length_mod_512 > 447 { 512 } else { 0 } - input_length_mod_512;
+
+    let number_of_padding_bytes: usize = ((number_of_zeros + 1) / 8) + 8;
+
+    output[number_of_padding_bytes - 8] = ((input_length_in_bits >> (7 * 8)) & 0xff) as u8;
+    output[number_of_padding_bytes - 7] = ((input_length_in_bits >> (6 * 8)) & 0xff) as u8;
+    output[number_of_padding_bytes - 6] = ((input_length_in_bits >> (5 * 8)) & 0xff) as u8;
+    output[number_of_padding_bytes - 5] = ((input_length_in_bits >> (4 * 8)) & 0xff) as u8;
+    output[number_of_padding_bytes - 4] = ((input_length_in_bits >> (3 * 8)) & 0xff) as u8;
+    output[number_of_padding_bytes - 3] = ((input_length_in_bits >> (2 * 8)) & 0xff) as u8;
+    output[number_of_padding_bytes - 2] = ((input_length_in_bits >> 8) & 0xff) as u8;
+    output[number_of_padding_bytes - 1] = ((input_length_in_bits) & 0xff) as u8;
+
+    (output, number_of_padding_bytes)
 }
 
 #[inline]
-fn make_u32s_from_u8s(chunk_u8: &[u8]) -> [u32; 16] {
-    debug_assert_eq!(chunk_u8.len(), 64);
+fn make_16xu32s_from_64xu8s(u8s: &[u8]) -> [u32; 16] {
+    debug_assert_eq!(u8s.len(), 64);
     [
-        u32::from_be_bytes([chunk_u8[0], chunk_u8[1], chunk_u8[2], chunk_u8[3]]),
-        u32::from_be_bytes([chunk_u8[4], chunk_u8[5], chunk_u8[6], chunk_u8[7]]),
-        u32::from_be_bytes([chunk_u8[8], chunk_u8[9], chunk_u8[10], chunk_u8[11]]),
-        u32::from_be_bytes([chunk_u8[12], chunk_u8[13], chunk_u8[14], chunk_u8[15]]),
-        u32::from_be_bytes([chunk_u8[16], chunk_u8[17], chunk_u8[18], chunk_u8[19]]),
-        u32::from_be_bytes([chunk_u8[20], chunk_u8[21], chunk_u8[22], chunk_u8[23]]),
-        u32::from_be_bytes([chunk_u8[24], chunk_u8[25], chunk_u8[26], chunk_u8[27]]),
-        u32::from_be_bytes([chunk_u8[28], chunk_u8[29], chunk_u8[30], chunk_u8[31]]),
-        u32::from_be_bytes([chunk_u8[32], chunk_u8[33], chunk_u8[34], chunk_u8[35]]),
-        u32::from_be_bytes([chunk_u8[36], chunk_u8[37], chunk_u8[38], chunk_u8[39]]),
-        u32::from_be_bytes([chunk_u8[40], chunk_u8[41], chunk_u8[42], chunk_u8[43]]),
-        u32::from_be_bytes([chunk_u8[44], chunk_u8[45], chunk_u8[46], chunk_u8[47]]),
-        u32::from_be_bytes([chunk_u8[48], chunk_u8[49], chunk_u8[50], chunk_u8[51]]),
-        u32::from_be_bytes([chunk_u8[52], chunk_u8[53], chunk_u8[54], chunk_u8[55]]),
-        u32::from_be_bytes([chunk_u8[56], chunk_u8[57], chunk_u8[58], chunk_u8[59]]),
-        u32::from_be_bytes([chunk_u8[60], chunk_u8[61], chunk_u8[62], chunk_u8[63]]),
+        u32::from_be_bytes([u8s[0], u8s[1], u8s[2], u8s[3]]),
+        u32::from_be_bytes([u8s[4], u8s[5], u8s[6], u8s[7]]),
+        u32::from_be_bytes([u8s[8], u8s[9], u8s[10], u8s[11]]),
+        u32::from_be_bytes([u8s[12], u8s[13], u8s[14], u8s[15]]),
+        u32::from_be_bytes([u8s[16], u8s[17], u8s[18], u8s[19]]),
+        u32::from_be_bytes([u8s[20], u8s[21], u8s[22], u8s[23]]),
+        u32::from_be_bytes([u8s[24], u8s[25], u8s[26], u8s[27]]),
+        u32::from_be_bytes([u8s[28], u8s[29], u8s[30], u8s[31]]),
+        u32::from_be_bytes([u8s[32], u8s[33], u8s[34], u8s[35]]),
+        u32::from_be_bytes([u8s[36], u8s[37], u8s[38], u8s[39]]),
+        u32::from_be_bytes([u8s[40], u8s[41], u8s[42], u8s[43]]),
+        u32::from_be_bytes([u8s[44], u8s[45], u8s[46], u8s[47]]),
+        u32::from_be_bytes([u8s[48], u8s[49], u8s[50], u8s[51]]),
+        u32::from_be_bytes([u8s[52], u8s[53], u8s[54], u8s[55]]),
+        u32::from_be_bytes([u8s[56], u8s[57], u8s[58], u8s[59]]),
+        u32::from_be_bytes([u8s[60], u8s[61], u8s[62], u8s[63]]),
     ]
 }
 
+/// SHA256 primative
 #[inline]
 fn ch(x: u32, y: u32, z: u32) -> u32 {
     (x & y) ^ (!x & z)
 }
 
+/// SHA256 primative
 #[inline]
 fn maj(x: u32, y: u32, z: u32) -> u32 {
     (x & y) ^ (x & z) ^ (y & z)
 }
 
+/// SHA256 primative
 #[inline]
 fn big_sigma_0(x: u32) -> u32 {
     x.rotate_right(2) ^ x.rotate_right(13) ^ x.rotate_right(22)
 }
 
+/// SHA256 primative
 #[inline]
 fn big_sigma_1(x: u32) -> u32 {
     x.rotate_right(6) ^ x.rotate_right(11) ^ x.rotate_right(25)
 }
 
+/// SHA256 primative
 #[inline]
 fn little_sigma_0(x: u32) -> u32 {
     x.rotate_right(7) ^ x.rotate_right(18) ^ (x >> 3)
 }
 
+/// SHA256 primative
 #[inline]
 fn little_sigma_1(x: u32) -> u32 {
     x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10)
-}
-
-#[inline]
-fn padded_input(input: &[u8]) -> Vec<u8> {
-    let input_length_in_bits = input.len() * 8;
-    let padding = get_padding_bytes(input_length_in_bits as u64);
-    let padding_iter = padding.iter();
-    let input_iter = input.iter();
-    let padded_input_chain = input_iter.chain(padding_iter);
-    padded_input_chain.cloned().collect::<Vec<u8>>()
-}
-
-#[inline]
-fn get_padding_bytes(input_length_in_bits: u64) -> Vec<u8> {
-    // number of zeros to get to a multiple of 512 bits total length, given
-    // we have to add a 1 and the length of the message in bits, as a big-endian
-    // u64.
-    let mut number_of_zeros: i16 = 447 - ((input_length_in_bits % 512) as i16);
-    if number_of_zeros < 0 {
-        number_of_zeros += 512;
-    }
-
-    let mut padding: Vec<u8> = Vec::with_capacity((((number_of_zeros + 1) / 8) + 8) as usize);
-
-    padding.push(1 << 7);
-
-    padding.append(&mut vec![0u8; ((number_of_zeros - 7) / 8) as usize]);
-
-    for shift in (0..=7).rev() {
-        padding.push(((input_length_in_bits >> (shift * 8)) & 0xff) as u8);
-    }
-
-    padding
 }
--- src/hash/sha256/tests.rs
+++ src/hash/sha256/tests.rs
@@ -1,102 +1,70 @@
 use super::*;
 
+fn assert_padding_is_correct(
+    expected_padding_size_bytes: usize,
+    unpadded_length_bits: u64,
+    actual_padding_tuple: ([u8; 72], usize),
+) {
+    let (actual_padding_bytes, actual_padding_size_bytes) = actual_padding_tuple;
+    let mut actual_length_in_bits_u8: [u8; 8] = [0; 8];
+    actual_length_in_bits_u8.copy_from_slice(
+        &actual_padding_bytes[expected_padding_size_bytes - 8..expected_padding_size_bytes],
+    );
+    assert_eq!(
+        actual_padding_size_bytes, expected_padding_size_bytes,
+        "Actual padding size in bytes (left) should equal expected padding size in bytes (right)"
+    );
+    assert_eq!(
+        actual_padding_bytes[0], 0x80,
+        "First byte of padding must be 0x80"
+    );
+    assert_eq!(
+        u64::from_be_bytes(actual_length_in_bits_u8),
+        unpadded_length_bits,
+        "Last 8 bytes of padding decoded as u64 should give unpadded length in bits"
+    );
+    for b in actual_padding_bytes[1..expected_padding_size_bytes - 8].iter() {
+        assert_eq!(*b, 0, "Padding bytes should be 0");
+    }
+}
+
 #[test]
 fn test_vectors() {
+    assert_padding_is_correct(61, 3 * 8, get_padding_bytes(3 * 8));
     assert_eq!(
-        padded_input(&String::from("abc").as_bytes()),
-        vec![
-            0x61, 0x62, 0x63, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18
-        ]
-    );
-
-    assert_eq!(
-        hash(&String::from("abc").as_bytes()),
+        hash(b"abc"),
         [
             0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea, 0x41, 0x41, 0x40, 0xde, 0x5d, 0xae,
             0x22, 0x23, 0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c, 0xb4, 0x10, 0xff, 0x61,
             0xf2, 0x00, 0x15, 0xad
-        ]
+        ],
+        "Hash of abc should be ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
     );
 
+    assert_padding_is_correct(72, 56 * 8, get_padding_bytes(56 * 8));
     assert_eq!(
-        padded_input(
-            &String::from("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq").as_bytes()
-        ),
-        vec![
-            0x61, 0x62, 0x63, 0x64, 0x62, 0x63, 0x64, 0x65, 0x63, 0x64, 0x65, 0x66, 0x64, 0x65,
-            0x66, 0x67, 0x65, 0x66, 0x67, 0x68, 0x66, 0x67, 0x68, 0x69, 0x67, 0x68, 0x69, 0x6a,
-            0x68, 0x69, 0x6a, 0x6b, 0x69, 0x6a, 0x6b, 0x6c, 0x6a, 0x6b, 0x6c, 0x6d, 0x6b, 0x6c,
-            0x6d, 0x6e, 0x6c, 0x6d, 0x6e, 0x6f, 0x6d, 0x6e, 0x6f, 0x70, 0x6e, 0x6f, 0x70, 0x71,
-            0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x01, 0xc0,
-        ]
-    );
-
-    assert_eq!(
-        hash(&String::from("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq").as_bytes()),
+        hash(b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"),
         [
             0x24, 0x8d, 0x6a, 0x61, 0xd2, 0x06, 0x38, 0xb8, 0xe5, 0xc0, 0x26, 0x93, 0x0c, 0x3e,
             0x60, 0x39, 0xa3, 0x3c, 0xe4, 0x59, 0x64, 0xff, 0x21, 0x67, 0xf6, 0xec, 0xed, 0xd4,
             0x19, 0xdb, 0x06, 0xc1
-        ]
+        ],
+        "Hash of abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq should be 248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"
     )
 }
 
 #[test]
-fn test_pad_input() {
-    fn get_vec_with_length(len: usize, filler: u8) -> Vec<u8> {
-        let mut v: Vec<u8> = Vec::with_capacity(len);
-        for _ in 0..len {
-            v.push(filler);
-        }
-        v
-    }
-
-    for len in 0..=500 {
-        let filler: u8 = (len % 256) as u8;
-        let v: Vec<u8> = get_vec_with_length(len, filler);
-        let length_in_bits = len * 8;
-
-        let actual = padded_input(&v);
-        let actual_length_in_bits = actual.len() * 8;
-
-        // Padded length is divisible by 512
-        assert!(
-            actual_length_in_bits % 512 == 0,
-            "Length {} not divisible by 512",
-            actual_length_in_bits
+fn test_padding() {
+    for len in 0..=512 {
+        let padding_tuple = get_padding_bytes(len * 8);
+        assert_eq!(
+            ((padding_tuple.1 * 8) + (len * 8)) % 512,
+            0,
+            "Length of padded value in bits must be a multiple of 512"
         );
-
-        // The first len bytes are the original value
-        for b in actual.iter().take(len) {
-            assert!(*b == filler, "Byte {} is not {}", *b, filler);
-        }
-
-        // The next byte is 0x80
-        assert!(actual[len] == 0x80);
-
-        // ... followed by zero or more 0x00 bytes
-        for b in actual.iter().take(actual.len() - 8).skip(len + 1) {
-            assert!(*b == 0x00);
-        }
-
-        // The last 8 bytes contain the length in bits
-        // as a big-endian 64 bit unsigned int
-        assert!(actual[actual.len() - 8] == ((length_in_bits >> 56) & 0xff) as u8);
-        assert!(actual[actual.len() - 7] == ((length_in_bits >> 48) & 0xff) as u8);
-        assert!(actual[actual.len() - 6] == ((length_in_bits >> 40) & 0xff) as u8);
-        assert!(actual[actual.len() - 5] == ((length_in_bits >> 32) & 0xff) as u8);
-        assert!(actual[actual.len() - 4] == ((length_in_bits >> 24) & 0xff) as u8);
-        assert!(actual[actual.len() - 3] == ((length_in_bits >> 16) & 0xff) as u8);
-        assert!(actual[actual.len() - 2] == ((length_in_bits >> 8) & 0xff) as u8);
-        assert!(actual[actual.len() - 1] == (length_in_bits & 0xff) as u8);
+        // We use padding_tuple.1 as expected_padding_size_bytes to avoid duplicating
+        // calcuations from get_padding_bytes() here (which wouldn't really test anything).
+        // There are a couple of tests for the accuracy of this in test_vectors().
+        assert_padding_is_correct(padding_tuple.1, len as u64 * 8, padding_tuple);
     }
 }
